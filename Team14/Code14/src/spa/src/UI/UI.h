e1fdc2e3137b65e31806e869e67089b172753de9
Chen Xihao <chenxihao0627@gmail.com>
2022-08-30 19:35:14 +0800

    Add UI component base code

---
+++ Team14/Code14/src/spa/src/UI/UI.h
@@ -0,0 +1,30 @@
+//
+// Created by Xihao Chen on 30/8/22.
+//
+#pragma once
+
+#include <string>
+
+
+using namespace std;
+
+
+#ifndef SPA_UI_H
+#define SPA_UI_H
+
+
+class UI {
+    string getSimpleProgramme();
+
+
+    string evaluateSimpleProgramme(string);
+
+
+    string evaluateQuery(string);
+
+
+    string display(string)
+};
+
+
+#endif //SPA_UI_H
