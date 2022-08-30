7599a87ab5775cfb373bef90ed2390a2a25c586a
Chen Xihao <chenxihao0627@gmail.com>
2022-08-30 19:22:26 +0800

    Add component interaction interface base code

---
+++ Team14/Code14/src/spa/src/Interface/PKBInterface.h
@@ -0,0 +1,14 @@
+//
+// Created by Xihao Chen on 30/8/22.
+//
+
+#ifndef SPA_PKBINTERFACE_H
+#define SPA_PKBINTERFACE_H
+
+
+class PKBInterface {
+
+};
+
+
+#endif //SPA_PKBINTERFACE_H
---
+++ Team14/Code14/src/spa/src/Interface/QPSInterface.h
@@ -0,0 +1,14 @@
+//
+// Created by Xihao Chen on 30/8/22.
+//
+
+#ifndef SPA_QPSINTERFACE_H
+#define SPA_QPSINTERFACE_H
+
+
+class QPSInterface {
+
+};
+
+
+#endif //SPA_QPSINTERFACE_H
