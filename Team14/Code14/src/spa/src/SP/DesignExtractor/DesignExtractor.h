448cc9923822498f93de486448ea5764569402c9
Chen Xihao <chenxihao0627@gmail.com>
2022-08-30 20:16:28 +0800

    Refactor DesignExtractor to SP

---
+++ Team14/Code14/src/spa/src/SP/DesignExtractor/DesignExtractor.h
@@ -0,0 +1,17 @@
+//
+// Created by Xihao Chen on 30/8/22.
+//
+
+#pragma once
+using namespace std;
+
+#ifndef SPA_DESIGNEXTRACTOR_H
+#define SPA_DESIGNEXTRACTOR_H
+
+
+class DesignExtractor {
+
+};
+
+
+#endif //SPA_DESIGNEXTRACTOR_H
