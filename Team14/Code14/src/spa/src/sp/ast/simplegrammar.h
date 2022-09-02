553b3b54dcb964220462d6211fa1c9e1c4bb607b
Smanmos <57220480+Smanmos@users.noreply.github.com>
2022-09-02 15:00:32 +0800

    Add classes for grammar rules of SIMPLE

---
+++ Team14/Code14/src/spa/src/sp/ast/simplegrammar.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include "simpleast.h"
+
+typedef std::vector<Token*>::iterator TokenIterator;
+
+class SimpleGrammarRule {
+ public:
+  virtual SimpleAstNode* parseNode(TokenIterator tokenStream) = 0;
+};
+
+class ProgramGrammarRule : public SimpleGrammarRule {
+ public:
+  ProgramNode* parseNode(TokenIterator tokenStream);
+};
+
+class ProcedureGrammarRule : public SimpleGrammarRule {
+ public:
+  ProcedureNode* parseNode(TokenIterator tokenStream);
+};
+
+class StatementListGrammarRule : public SimpleGrammarRule {
+ public:
+  StatementListNode* parseNode(TokenIterator tokenStream);
+};
+
+class StatementGrammarRule : public SimpleGrammarRule {
+ public:
+  StatementNode* parseNode(TokenIterator tokenStream);
+};
+
+class StatementGrammarRule : public SimpleGrammarRule {
+ public:
+  StatementNode* parseNode(TokenIterator tokenStream);
+};
+
+class ReadGrammarRule : public SimpleGrammarRule {
+ public:
+  ReadNode* parseNode(TokenIterator tokenStream);
+};
+
+class PrintGrammarRule : public SimpleGrammarRule {
+ public:
+  PrintNode* parseNode(TokenIterator tokenStream);
+};
+
+class CallGrammarRule : public SimpleGrammarRule {
+ public:
+  CallNode* parseNode(TokenIterator tokenStream);
+};
+
+class WhileGrammarRule : public SimpleGrammarRule {
+ public:
+  WhileNode* parseNode(TokenIterator tokenStream);
+};
+
+class IfGrammarRule : public SimpleGrammarRule {
+ public:
+  IfNode* parseNode(TokenIterator tokenStream);
+};
+
+class AssignGrammarRule : public SimpleGrammarRule {
+ public:
+  AssignNode* parseNode(TokenIterator tokenStream);
+};
+
+class CondExprGrammarRule : public SimpleGrammarRule {
+ public:
+  CondExprNode* parseNode(TokenIterator tokenStream);
+};
+
+class RelExprGrammarRule : public SimpleGrammarRule {
+ public:
+  RelExprNode* parseNode(TokenIterator tokenStream);
+};
+
+class ExprGrammarRule : public SimpleGrammarRule {
+ public:
+  ExprNode* parseNode(TokenIterator tokenStream);
+};
+
+class TermGrammarRule : public SimpleGrammarRule {
+ public:
+  ExprNode* parseNode(TokenIterator tokenStream);
+};
+
+class FactorGrammarRule : public SimpleGrammarRule {
+ public:
+  ExprNode* parseNode(TokenIterator tokenStream);
+};
+
+class ReferenceGrammarRule : public SimpleGrammarRule {
+ public:
+  ReferenceNode* parseNode(TokenIterator tokenStream);
+};
+
+class VariableGrammarRule : public SimpleGrammarRule {
+ public:
+  VariableNode* parseNode(TokenIterator tokenStream);
+};
+
+class ConstantGrammarRule : public SimpleGrammarRule {
+ public:
+  ConstantNode* parseNode(TokenIterator tokenStream);
+};
