6ca19746564f5170cd75e835ed5e3d183363f61c
Chen Xihao <chenxihao0627@gmail.com>
2022-09-04 17:42:36 +0800

    feat: add results class

--- Team14/Code14/src/spa/src/commons/evaluator/result.cpp
+++ Team14/Code14/src/spa/src/commons/evaluator/result.cpp
@@ -1,1 +1,19 @@
 #include "result.h"
+
+Result Result::empty(QuerySynonym &syn) {
+  std::vector<std::string> empty;
+  return Result(syn, empty, true);
+}
+
+bool Result::is_empty() {
+  return this->is_empty_;
+}
+
+QuerySynonym Result::get_synonym() {
+  return this->syn_;
+}
+
+std::vector<std::string> Result::get_results_list() {
+  return this->results_;
+}
+
--- Team14/Code14/src/spa/src/commons/evaluator/result.h
+++ Team14/Code14/src/spa/src/commons/evaluator/result.h
@@ -5,11 +5,23 @@
 #include "qps/pql/query_synonym/query_synonym.h"
 class Result {
  private:
+  bool is_empty_ = true;
+
   QuerySynonym syn_;
 
   std::vector<std::string> results_;
 
- public:
-  explicit Result(QuerySynonym &syn, std::vector<std::string> &results) : syn_(syn), results_(results) {};
+ protected:
+  explicit Result(QuerySynonym &syn, std::vector<std::string> &results, bool is_empty)
+      : syn_(syn), results_(results), is_empty_(is_empty) {};
 
+ public:
+  explicit Result(QuerySynonym &syn, std::vector<std::string> &results)
+      : syn_(syn), results_(results), is_empty_(false) {};
+
+  static Result empty(QuerySynonym &);
+
+  bool is_empty();
+  QuerySynonym get_synonym();
+  std::vector<std::string> get_results_list();
 };
