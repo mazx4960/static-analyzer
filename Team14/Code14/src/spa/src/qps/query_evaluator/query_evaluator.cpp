2d4c94791630fc0224ca624b9b35b1948754cbdf
Chen Xihao <chenxihao0627@gmail.com>
2022-09-04 17:43:20 +0800

    feat: implement query evaluator

--- Team14/Code14/src/spa/src/qps/query_evaluator/query_evaluator.cpp
+++ Team14/Code14/src/spa/src/qps/query_evaluator/query_evaluator.cpp
@@ -1,3 +1,19 @@
 // Copyright 2022 CS3203 Team14. All rights reserved.
 
 #include "query_evaluator.h"
+
+Result QueryEvaluator::evaluate() {
+  for (QueryCall &query_call : this->query_.getQueryCall()) {
+    SubQueryEvaluator sub_query_evaluator = SubQueryEvaluator(this->pkb_interface_, query_call);
+
+    Result partial_result = sub_query_evaluator.evaluate();
+    this->partial_results_list_.push_back(partial_result);
+  }
+
+  if (this->partial_results_list_.size() == 1) {
+    return this->partial_results_list_[0];
+  }
+
+  // TODO: implement projection and aggregation
+  return this->partial_results_list_[0];
+}
--- Team14/Code14/src/spa/src/qps/query_evaluator/query_evaluator.h
+++ Team14/Code14/src/spa/src/qps/query_evaluator/query_evaluator.h
@@ -1,4 +1,19 @@
 // Copyright 2022 CS3203 Team14. All rights reserved.
 #pragma once
 
-class query_evaluator {};
+#include "evaluation_strategy.h"
+#include "subquery_evaluator.h"
+
+class QueryEvaluator {
+ private:
+  Query query_;
+
+  PKBInterface pkb_interface_;
+
+  std::vector<Result> partial_results_list_;
+
+ public:
+  QueryEvaluator(PKBInterface &pkb_interface, Query &query) : pkb_interface_(pkb_interface), query_(query) {};
+
+  Result evaluate();
+};
