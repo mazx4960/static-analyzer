161702f247075e25ae86c6c151522eddc8cf1815
Chen Xihao <chenxihao0627@gmail.com>
2022-09-06 04:56:11 +0800

    test: add QE unit test

--- Team14/Code14/src/unit_testing/src/qps/qe_test.cpp
+++ Team14/Code14/src/unit_testing/src/qps/qe_test.cpp
@@ -4,7 +4,7 @@
 #include "pkb/pkb.h"
 #include "qps/query_evaluator/query_evaluator.h"
 
-TEST(QueryEvaluatorGetTest, TestBasic) {
+TEST(QETest, VariableGetTest) {
   PKB *pkb = new PKB();
   std::vector<Entity *> entities = {
       new VariableEntity("x"),
@@ -23,6 +23,9 @@ TEST(QueryEvaluatorGetTest, TestBasic) {
 
   QueryEvaluator query_evaluator = QueryEvaluator(pkb, query);
   Result result = query_evaluator.evaluate();
+  std::unordered_set<std::string> result_set = result.get_results();
+  std::vector<std::string> result_vector(result_set.begin(), result_set.end());
+  std::sort(result_vector.begin(), result_vector.end());
 
-  ASSERT_EQ(result.get_results(), std::unordered_set<std::string>({"x", "y", "z"}));
+  ASSERT_EQ(result_vector, std::vector<std::string>({"x", "y", "z"}));
 }
