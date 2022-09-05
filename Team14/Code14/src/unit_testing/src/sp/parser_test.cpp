27b4c5fc4b2e0e3e2a1d62fc2595a34f18e14dd5
Smanmos <57220480+Smanmos@users.noreply.github.com>
2022-09-05 22:55:48 +0800

    feat: expand on test case

--- Team14/Code14/src/unit_testing/src/sp/parser_test.cpp
+++ Team14/Code14/src/unit_testing/src/sp/parser_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "commons/lexer/token.h"
+#include "commons/types.h"
 #include "sp/ast/simpleast.h"
 #include "sp/ast/simplenodetype.h"
 #include "sp/grammar/simplegrammar.h"
@@ -14,5 +15,27 @@ TEST(SimpleParserTest, TestBasic) {
       new EndOfFileToken()};
   auto iter = input_tokens.begin();
   SimpleAstNode* program_node = (new ProgramGrammarRule())->parseNode(iter);
-  ASSERT_EQ(program_node->GetNodeType(),SimpleNodeType::kProgram);
+  ASSERT_EQ(SimpleNodeType::kProgram, program_node->GetNodeType());
+  auto program_children = program_node->GetChildren();
+  ASSERT_EQ(1, program_children.size());
+  auto *procedure_node = program_children[0];
+  ASSERT_EQ(SimpleNodeType::kProcedure, procedure_node->GetNodeType());
+  ASSERT_EQ("main", static_cast<ProcedureNode*>(procedure_node)->GetProcName());
+  auto procedure_children = procedure_node->GetChildren();
+  ASSERT_EQ(1, procedure_children.size());
+  auto *statement_list_node = procedure_children[0];
+  ASSERT_EQ(SimpleNodeType::kStatementList, statement_list_node->GetNodeType());
+  auto statement_children = statement_list_node->GetChildren();
+  ASSERT_EQ(1, statement_children.size());
+  auto *statement_node = statement_children[0];
+  ASSERT_EQ(SimpleNodeType::kStatement, statement_node->GetNodeType());
+  ASSERT_EQ(StmtType::kAssign, static_cast<StatementNode*>(statement_node)->GetStmtType());
+  auto assign_children = statement_node->GetChildren();
+  ASSERT_EQ(2, assign_children.size());
+  auto *variable_node = assign_children[0];
+  ASSERT_EQ(SimpleNodeType::kVariable, variable_node->GetNodeType());
+  ASSERT_EQ("x", static_cast<VariableNode*>(variable_node)->GetVariableName());
+  auto *constant_node = assign_children[1];
+  ASSERT_EQ(SimpleNodeType::kConstant, constant_node->GetNodeType());
+  ASSERT_EQ(1, static_cast<ConstantNode*>(constant_node)->GetValue());
 }
