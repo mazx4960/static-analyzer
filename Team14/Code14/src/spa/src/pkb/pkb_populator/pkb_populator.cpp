bc9a814a087c434eb6156dfb9b0f222257e822bd
mazx4960 <desmondtan4561@gmail.com>
2022-09-05 23:28:28 +0800

    fix: remove unnecessary imports

--- Team14/Code14/src/spa/src/pkb/pkb_populator/pkb_populator.cpp
+++ Team14/Code14/src/spa/src/pkb/pkb_populator/pkb_populator.cpp
@@ -2,17 +2,6 @@
 
 #include "pkb/pkb_populator/pkb_populator.h"
 
-#include <unordered_set>
-#include <vector>
-#include <map>
-#include <string>
-#include <utility>
-#include <vector>
-
-#include "commons/entity.h"
-#include "commons/types.h"
-#include "pkb/entity/entity_manager.h"
-
 EntityManager entity_manager;
 
 std::unordered_set<std::string>* PKBPopulator::populate(const std::vector<Entity>& entities) {
