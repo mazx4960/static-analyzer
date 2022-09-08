0d50b5e51082aa4b19c1ee35843d8e39f2f70c78
KelvinSoo <e0544272@u.nus.edu.sg>
2022-09-08 17:20:30 +0800

    fix: seperate builder from validator

--- Team14/Code14/src/spa/src/qps/query_evaluator/query_evaluator.cpp
+++ Team14/Code14/src/spa/src/qps/query_evaluator/query_evaluator.cpp
@@ -2,29 +2,29 @@
 
 Result QueryEvaluator::evaluate() {
   // Query declaration for whose results are to be returned.
-  QueryDeclaration called_declaration = this->query_.getQueryCall().getDeclaration();
-  QuerySynonym query_synonym = called_declaration.getSynonym();
+  QueryDeclaration* called_declaration = this->query_.getQueryCall().getDeclaration();
+  QuerySynonym query_synonym = called_declaration->getSynonym();
 
   // Declarations in the query.
-  std::vector<QueryDeclaration> query_declarations = this->query_.getDeclarations();
+  std::vector<QueryDeclaration*> query_declarations = this->query_.getDeclarations();
 
   // Sub-queries that need to be processed.
   std::vector<QueryClause> subquery_clauses = this->query_.getQueryCall().getClauseVector();
 
-  for (QueryDeclaration &declaration : query_declarations) {
+  for (QueryDeclaration* declaration : query_declarations) {
 
-    QuerySynonym declaration_synonym = declaration.getSynonym();
-    Entity variable_entity = declaration.getEntity();
+    QuerySynonym declaration_synonym = declaration->getSynonym();
+    Entity* variable_entity = declaration->getEntity();
 
-    PKBQuery pkb_query = *PKBQuery::getQuery(variable_entity);
+    PKBQuery pkb_query = *PKBQuery::getQuery(*variable_entity);
     pkb_query.setSynonym(declaration_synonym);
 
     Result query_result = this->pkb_->getResult(pkb_query);
-    this->context_.insert({declaration, query_result.get_results_set()});
+    this->context_.insert({*declaration, query_result.get_results_set()});
   }
 
   if (subquery_clauses.empty()) {
-    return Result(query_synonym, this->context_.at(called_declaration));
+    return Result(query_synonym, this->context_.at(*called_declaration));
   }
 
 
