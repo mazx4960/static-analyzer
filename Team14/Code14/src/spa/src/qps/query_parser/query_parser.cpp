b71af3009b580aa870b4fa0b47bd9c1d22ee51c6
KelvinSoo <e0544272@u.nus.edu.sg>
2022-09-08 21:19:17 +0800

    fix: missing validate statement in parser

--- Team14/Code14/src/spa/src/qps/query_parser/query_parser.cpp
+++ Team14/Code14/src/spa/src/qps/query_parser/query_parser.cpp
@@ -5,10 +5,13 @@
 #include "query_builder.h"
 #include "query_lexer.h"
 #include "query_validator/query_validator.h"
+#include "spdlog/spdlog.h"
 
 Query QueryParser::parse(std::ifstream *query_stream) {
   QueryLexer lexer = QueryLexer(query_stream);
   QueryValidator validator = QueryValidator(lexer.lex());
+  validator.validate();
+  spdlog::info("Tokens validated");
   QueryBuilder builder = QueryBuilder();
   builder.withDeclarations(validator.getDeclarations());
   builder.withQueryCalls(validator.getQueryCalls());
