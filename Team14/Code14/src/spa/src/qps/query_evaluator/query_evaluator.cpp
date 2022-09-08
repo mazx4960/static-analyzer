2d39851ddf010c33c39de6a46c3cbc8b65a58bc3
Chen Xihao <chenxihao0627@gmail.com>
2022-09-08 20:09:56 +0800

    feat: refactor evaluator

--- Team14/Code14/src/spa/src/qps/query_evaluator/query_evaluator.cpp
+++ Team14/Code14/src/spa/src/qps/query_evaluator/query_evaluator.cpp
@@ -1,27 +1,35 @@
 #include "query_evaluator.h"
 
-Result QueryEvaluator::evaluate() {
+void QueryEvaluator::fetchContext() {
   // Query declaration for whose results are to be returned.
-  QueryDeclaration* called_declaration = this->query_.getQueryCall().getDeclaration();
+  QueryDeclaration *called_declaration = this->query_.getQueryCall().getDeclaration();
   QuerySynonym query_synonym = called_declaration->getSynonym();
 
   // Declarations in the query.
-  std::vector<QueryDeclaration*> query_declarations = this->query_.getDeclarations();
+  std::vector<QueryDeclaration *> query_declarations = this->query_.getDeclarations();
 
-  // Sub-queries that need to be processed.
-  std::vector<QueryClause> subquery_clauses = this->query_.getQueryCall().getClauseVector();
-
-  for (QueryDeclaration* declaration : query_declarations) {
+  for (QueryDeclaration *declaration : query_declarations) {
 
     QuerySynonym declaration_synonym = declaration->getSynonym();
-    Entity* variable_entity = declaration->getEntity();
+    Entity *variable_entity = declaration->getEntity();
 
     PKBQuery pkb_query = *PKBQuery::getQuery(*variable_entity);
     pkb_query.setSynonym(declaration_synonym);
 
-    Result query_result = this->pkb_->getResult(pkb_query);
+    Result query_result = this->pkb_->get(pkb_query);
     this->context_.insert({*declaration, query_result.get_results_set()});
   }
+}
+
+Result QueryEvaluator::evaluate() {
+  this->fetchContext();
+  
+  // Query declaration for whose results are to be returned.
+  QueryDeclaration *called_declaration = this->query_.getQueryCall().getDeclaration();
+  QuerySynonym query_synonym = called_declaration->getSynonym();
+
+  // Sub-queries that need to be processed.
+  std::vector<QueryClause> subquery_clauses = this->query_.getQueryCall().getClauseVector();
 
   if (subquery_clauses.empty()) {
     return Result(query_synonym, this->context_.at(*called_declaration));
