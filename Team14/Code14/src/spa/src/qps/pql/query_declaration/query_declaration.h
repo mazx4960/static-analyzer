f4e3754a9927f3e512563e679ee5d0515568d1ef
KelvinSoo <e0544272@u.nus.edu.sg>
2022-09-09 14:53:00 +0800

    feat: add new query declaration header

--- Team14/Code14/src/spa/src/qps/pql/query_declaration/query_declaration.h
+++ Team14/Code14/src/spa/src/qps/pql/query_declaration/query_declaration.h
@@ -8,23 +8,112 @@
 #include "commons/entity.h"
 #include "qps/pql/query_synonym/query_synonym.h"
 
+enum class DeclarationType {
+  kStatement,
+  kRead,
+  kPrint,
+  kCall,
+  kWhile,
+  kIf,
+  kAssign,
+  kVariable,
+  kConstant,
+  kProcedure,
+  kWildcard,
+  kString,
+  kInteger,
+};
+
+
 class QueryDeclaration {
  private:
-  Entity* entity_;
-  QuerySynonym synonym_;
+  DeclarationType type_;
 
  public:
+  QueryDeclaration(DeclarationType type) : type_(std::move(type)) {};
 
-  QueryDeclaration(Entity* entity,
-                   QuerySynonym synonym) : entity_(std::move(entity)), synonym_(std::move(synonym)) {};
-
-  [[nodiscard]] Entity* getEntity() const;
-  [[nodiscard]] QuerySynonym getSynonym() const;
-
+  [[nodiscard]] DeclarationType * getType() const;
   bool operator==(const QueryDeclaration &other) const;
 };
 
-class QueryDeclarationHashFunction {
+class StatementDeclaration : public QueryDeclaration {
  public:
-  size_t operator()(const QueryDeclaration &) const;
+  QuerySynonym query_synonym_;
+  explicit StatementDeclaration(QuerySynonym query_synonym)
+      : QueryDeclaration(DeclarationType::kStatement), query_synonym_(std::move(query_synonym)) {}
+};
+
+class ReadDeclaration : public QueryDeclaration {
+ public:
+  QuerySynonym query_synonym_;
+  explicit ReadDeclaration(QuerySynonym query_synonym)
+      : QueryDeclaration(DeclarationType::kRead), query_synonym_(std::move(query_synonym)) {}
+};
+
+class PrintDeclaration : public QueryDeclaration {
+ public:
+  QuerySynonym query_synonym_;
+  explicit PrintDeclaration(QuerySynonym query_synonym)
+      : QueryDeclaration(DeclarationType::kPrint), query_synonym_(std::move(query_synonym)) {}
+};
+
+class CallDeclaration : public QueryDeclaration {
+ public:
+  QuerySynonym query_synonym_;
+  explicit CallDeclaration(QuerySynonym query_synonym)
+      : QueryDeclaration(DeclarationType::kCall), query_synonym_(std::move(query_synonym)) {}
+};
+
+class WhileDeclaration : public QueryDeclaration {
+ public:
+  QuerySynonym query_synonym_;
+  explicit WhileDeclaration(QuerySynonym query_synonym)
+      : QueryDeclaration(DeclarationType::kWhile), query_synonym_(std::move(query_synonym)) {}
+};
+class IfDeclaration : public QueryDeclaration {
+ public:
+  QuerySynonym query_synonym_;
+  explicit IfDeclaration(QuerySynonym query_synonym)
+      : QueryDeclaration(DeclarationType::kIf), query_synonym_(std::move(query_synonym)) {}
+};
+class AssignDeclaration : public QueryDeclaration {
+ public:
+  QuerySynonym query_synonym_;
+  explicit AssignDeclaration(QuerySynonym query_synonym)
+      : QueryDeclaration(DeclarationType::kAssign), query_synonym_(std::move(query_synonym)) {}
+};
+class VariableDeclaration : public QueryDeclaration {
+ public:
+  QuerySynonym query_synonym_;
+  explicit VariableDeclaration(QuerySynonym query_synonym)
+      : QueryDeclaration(DeclarationType::kVariable), query_synonym_(std::move(query_synonym)) {}
+};
+class ConstantDeclaration : public QueryDeclaration {
+ public:
+  QuerySynonym query_synonym_;
+  explicit ConstantDeclaration(QuerySynonym query_synonym)
+      : QueryDeclaration(DeclarationType::kConstant), query_synonym_(std::move(query_synonym)) {}
+};
+class ProcedureDeclaration : public QueryDeclaration {
+ public:
+  QuerySynonym query_synonym_;
+  explicit ProcedureDeclaration(QuerySynonym query_synonym)
+      : QueryDeclaration(DeclarationType::kProcedure), query_synonym_(std::move(query_synonym)) {}
+};
+class WildCardDeclaration : public QueryDeclaration {
+ public:
+  explicit WildCardDeclaration()
+  : QueryDeclaration(DeclarationType::kWildcard) {}
+};
+class StringDeclaration : public QueryDeclaration {
+ public:
+  std::string string_;
+  explicit StringDeclaration(std::string string)
+      : QueryDeclaration(DeclarationType::kString), string_(std::move(string)) {}
+};
+class IntegerDeclaration : public QueryDeclaration {
+ public:
+  int number_;
+  explicit IntegerDeclaration(int number)
+      : QueryDeclaration(DeclarationType::kInteger), number_(number) {}
 };
