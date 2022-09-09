2087748e63ea9420ecc389bd199f6aa57e164d1f
KelvinSoo <e0544272@u.nus.edu.sg>
2022-09-09 15:23:16 +0800

    feat: add comments to query declaration header

--- Team14/Code14/src/spa/src/qps/pql/query_declaration/query_declaration.h
+++ Team14/Code14/src/spa/src/qps/pql/query_declaration/query_declaration.h
@@ -20,17 +20,17 @@ enum class DeclarationType {
   kConstant,
   kProcedure,
   kWildcard,
+  kExpression,
   kString,
   kInteger,
 };
-
 
 class QueryDeclaration {
  private:
   DeclarationType type_;
   QuerySynonym query_synonym_ = QuerySynonym("");
   std::string string_;
-  int number_ = 0;
+  int number_;
 
  public:
   explicit QueryDeclaration(DeclarationType type) : type_(std::move(type)) {};
@@ -47,70 +47,98 @@ class QueryDeclaration {
   bool operator==(const QueryDeclaration &other) const;
 };
 
+// Pre-call declaration of "stmt x;" where x is the synonym
 class StatementDeclaration : public QueryDeclaration {
  public:
   explicit StatementDeclaration(QuerySynonym query_synonym)
       : QueryDeclaration(DeclarationType::kStatement, std::move(query_synonym)){}
 };
 
+// Pre-call declaration of "read x;" where x is the synonym
 class ReadDeclaration : public QueryDeclaration {
  public:
   explicit ReadDeclaration(QuerySynonym query_synonym)
       : QueryDeclaration(DeclarationType::kRead, std::move(query_synonym)) {}
 };
 
+// Pre-call declaration of "print x;" where x is the synonym
 class PrintDeclaration : public QueryDeclaration {
  public:
   explicit PrintDeclaration(QuerySynonym query_synonym)
       : QueryDeclaration(DeclarationType::kPrint, std::move(query_synonym)) {}
 };
 
+// Pre-call declaration of "call x;" where x is the synonym
 class CallDeclaration : public QueryDeclaration {
  public:
   explicit CallDeclaration(QuerySynonym query_synonym)
       : QueryDeclaration(DeclarationType::kCall, std::move(query_synonym)) {}
 };
 
+// Pre-call declaration of "if x;" where x is the synonym
 class WhileDeclaration : public QueryDeclaration {
  public:
   explicit WhileDeclaration(QuerySynonym query_synonym)
       : QueryDeclaration(DeclarationType::kWhile, std::move(query_synonym)) {}
 };
+
+// Pre-call declaration of "if x;" where x is the synonym
 class IfDeclaration : public QueryDeclaration {
  public:
   explicit IfDeclaration(QuerySynonym query_synonym)
       : QueryDeclaration(DeclarationType::kIf, std::move(query_synonym)) {}
 };
+
+// Pre-call declaration of "assign x;" where x is the synonym
 class AssignDeclaration : public QueryDeclaration {
  public:
   explicit AssignDeclaration(QuerySynonym query_synonym)
       : QueryDeclaration(DeclarationType::kAssign, std::move(query_synonym)) {}
 };
+
+// Pre-call declaration of "variable x;" where x is the synonym
 class VariableDeclaration : public QueryDeclaration {
  public:
   explicit VariableDeclaration(QuerySynonym query_synonym)
       : QueryDeclaration(DeclarationType::kVariable, std::move(query_synonym)) {}
 };
+
+// Pre-call declaration of "constant x;" where x is the synonym
 class ConstantDeclaration : public QueryDeclaration {
  public:
   explicit ConstantDeclaration(QuerySynonym query_synonym)
       : QueryDeclaration(DeclarationType::kConstant, std::move(query_synonym)) {}
 };
+
+// Pre-call declaration of "procedure x;" where x is the synonym
 class ProcedureDeclaration : public QueryDeclaration {
  public:
   explicit ProcedureDeclaration(QuerySynonym query_synonym)
       : QueryDeclaration(DeclarationType::kProcedure, std::move(query_synonym)) {}
 };
+
+// Inline declaration of  Wildcard "_"
 class WildCardDeclaration : public QueryDeclaration {
  public:
   explicit WildCardDeclaration()
   : QueryDeclaration(DeclarationType::kWildcard) {}
 };
+
+// Inline declaration of Expression "(x + (y * z))"
+class ExpressionDeclaration : public QueryDeclaration {
+ public:
+  explicit ExpressionDeclaration(std::string string)
+      : QueryDeclaration(DeclarationType::kExpression , std::move(string)) {}
+};
+
+// Inline declaration of string eg. Modifies("x", v)
 class StringDeclaration : public QueryDeclaration {
  public:
   explicit StringDeclaration(std::string string)
       : QueryDeclaration(DeclarationType::kString , std::move(string)) {}
 };
+
+// Inline declaration of integer eg. Modifies(3, v)
 class IntegerDeclaration : public QueryDeclaration {
  public:
   explicit IntegerDeclaration(int number)
