f6ef9b1b8d3e92c61e5339bccb3b49809ff188b4
Chen Xihao <chenxihao0627@gmail.com>
2022-09-11 22:26:17 +0800

    test: delete redundant test

--- Team14/Code14/src/unit_testing/src/pkb/pkb_query_test.cpp
+++ Team14/Code14/src/unit_testing/src/pkb/pkb_query_test.cpp
@@ -1,18 +1,8 @@
 // Copyright 2022 CS3203 Team14. All rights reserved.
 
-#include "commons/pkb_query.h"
 
 #include "commons/result.h"
 #include "gtest/gtest.h"
 #include "pkb/pkb.h"
 
-TEST(VarQueryTest, EmptyResult) {
-  PKB pkb;
-  QuerySynonym placeholder = QuerySynonym("placeholder");
-  Result result = pkb.getResults(EntityType::kConstant, placeholder);
-
-  std::unordered_set<std::string> v_result = result.get_results_set();
-  std::unordered_set<std::string> empty_result = Result::empty(placeholder).get_results_set();
-
-  ASSERT_EQ(v_result, empty_result);
-}
+// TODO(howtoosee, LeeEnEn): add PKB query test
