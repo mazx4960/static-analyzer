e12cc153724b32883db58e5a1bec55e72a5bb11e
Chen Xihao <chenxihao0627@gmail.com>
2022-09-11 23:50:14 +0800

    feat: update evaluation strategy

--- Team14/Code14/src/spa/src/qps/query_evaluator/evaluation_strategy.cpp
+++ Team14/Code14/src/spa/src/qps/query_evaluator/evaluation_strategy.cpp
@@ -12,14 +12,39 @@ EvaluationStrategy *EvaluationStrategy::getStrategy(IPKBQuerier *pkb, QueryClaus
 }
 
 // TODO(howtoosee) implement SuchThat strategy
-std::unordered_set<QueryDeclaration *> SuchThatStrategy::evaluate() {
-  QuerySynonym placeholder_syn("placeholder");
+void SuchThatStrategy::evaluate() {
+  // TODO(howtoosee, KelvinSoo) split strategy into 3 types: first is synonym, second is synonym, both are synonyms.
 
-  return std::unordered_set<QueryDeclaration *>();
+  SuchThatType query_type = this->clause_.getSuchThatType();
+  RsType rs_type; // TODO(howtoosee) convert SuchThatType query_type to RsType rs_type
+
+  QueryDeclaration *first_param = this->clause_.getFirst();
+  QueryDeclaration *second_param = this->clause_.getSecond();
+
+  QueryDeclaration *first_to_evaluate;
+  QueryDeclaration *second_to_evaluate;
+
+  if (first_param->getContext().size() <= second_param->getContext().size()) {
+    first_to_evaluate = first_param;
+    second_to_evaluate = second_param;
+  } else {
+    first_to_evaluate = second_param;
+    second_to_evaluate = first_param;
+  }
+
+  for (auto *context_entity : first_to_evaluate->getContext()) {
+    std::unordered_set<Entity *> second_results = this->pkb_->getByRelationship(rs_type, context_entity, false);
+    if (second_results.empty()) {
+      first_to_evaluate->getContext().erase(context_entity);
+    }
+    std::unordered_set<Entity *> results;
+
+    std::set_intersection(second_results.begin(), second_results.end(),
+                          second_to_evaluate->getContext().begin(), second_to_evaluate->getContext().end(),
+                          std::inserter(results, results.begin()));
+  }
 }
 
-std::unordered_set<QueryDeclaration *> PatternStrategy::evaluate() {
+void PatternStrategy::evaluate() {
   QuerySynonym placeholder_syn("placeholder");
-
-  return std::unordered_set<QueryDeclaration *>();
 }
--- Team14/Code14/src/spa/src/qps/query_evaluator/evaluation_strategy.h
+++ Team14/Code14/src/spa/src/qps/query_evaluator/evaluation_strategy.h
@@ -13,7 +13,7 @@ class EvaluationStrategy {
   explicit EvaluationStrategy(IPKBQuerier *pkb) : pkb_(pkb) {};
  public:
   static EvaluationStrategy *getStrategy(IPKBQuerier *, QueryClause &);
-  virtual std::unordered_set<QueryDeclaration *> evaluate() = 0;
+  virtual void evaluate() = 0;
 };
 
 /*
@@ -27,7 +27,7 @@ class SuchThatStrategy : public EvaluationStrategy {
   SuchThatStrategy(IPKBQuerier *pkb, SuchThatClause &query_clause)
       : EvaluationStrategy(pkb), clause_(query_clause) {};
 
-  std::unordered_set<QueryDeclaration *> evaluate() override;
+  void evaluate() override;
 };
 
 class PatternStrategy : public EvaluationStrategy {
@@ -37,7 +37,7 @@ class PatternStrategy : public EvaluationStrategy {
   PatternStrategy(IPKBQuerier *pkb, PatternClause &query_clause)
       : EvaluationStrategy(pkb), clause_(query_clause) {};
 
-  std::unordered_set<QueryDeclaration *> evaluate() override;
+  void evaluate() override;
 };
 
 class EvaluationStrategyCreationError : public std::runtime_error {
