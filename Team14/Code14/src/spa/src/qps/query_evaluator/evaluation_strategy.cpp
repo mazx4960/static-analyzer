85fe15b0bddf2cce806b8355769e47f4897a2260
mazx4960 <desmondtan4561@gmail.com>
2022-09-12 20:59:59 +0800

    fix: windows build error

--- Team14/Code14/src/spa/src/qps/query_evaluator/evaluation_strategy.cpp
+++ Team14/Code14/src/spa/src/qps/query_evaluator/evaluation_strategy.cpp
@@ -1,19 +1,19 @@
 #include "evaluation_strategy.h"
 
-/*
+/**
  * Factory method
  */
 EvaluationStrategy *EvaluationStrategy::getStrategy(IPKBQuerier *pkb, QueryClause &query_clause) {
   switch (query_clause.getClauseType()) {
-    case ClauseType::kSuchThat: return new SuchThatStrategy(pkb, static_cast<SuchThatClause &> (query_clause));
-    case ClauseType::kPattern:return new PatternStrategy(pkb, static_cast<PatternClause &> (query_clause));
+    case ClauseType::kSuchThat: return new SuchThatStrategy(pkb, static_cast<SuchThatClause &>(query_clause));
+    case ClauseType::kPattern: return new PatternStrategy(pkb, static_cast<PatternClause &>(query_clause));
     default: throw EvaluationStrategyCreationError("Invalid query clause type");
   }
 }
 
 // TODO(howtoosee) implement SuchThat strategy
 void SuchThatStrategy::evaluate() {
-  RsType rs_type; // TODO (howtoosee) translate clause type to rs_type
+  RsType rs_type;// TODO (howtoosee) translate clause type to rs_type
 
   QueryDeclaration *first_param = this->clause_.getFirst();
   QueryDeclaration *second_param = this->clause_.getSecond();
@@ -32,10 +32,8 @@ void SuchThatStrategy::evaluate() {
   }
 }
 
-void SuchThatStrategy::intersectContext(QueryDeclaration *param_to_send,
-                                        QueryDeclaration *param_to_be_intersected,
-                                        RsType rs_type,
-                                        bool invert_search) {
+void SuchThatStrategy::intersectContext(QueryDeclaration *param_to_send, QueryDeclaration *param_to_be_intersected,
+                                        RsType rs_type, bool invert_search) {
   std::unordered_set<Entity *> valid_entities_for_other_param;
 
   for (auto *context_entity : param_to_send->getContext()) {
@@ -43,9 +41,7 @@ void SuchThatStrategy::intersectContext(QueryDeclaration *param_to_send,
     if (valid_entities.empty()) {
       param_to_send->getContext().erase(context_entity);
     } else {
-      for (auto *entity : valid_entities) {
-        valid_entities_for_other_param.insert(entity);
-      }
+      for (auto *entity : valid_entities) { valid_entities_for_other_param.insert(entity); }
     }
   }
 
@@ -65,8 +61,7 @@ void PatternStrategy::evaluate() {
 
   intersectContext(first_param, second_param, third_param);
 }
-void PatternStrategy::intersectContext(QueryDeclaration *assign_param,
-                                       QueryDeclaration *left_param,
+void PatternStrategy::intersectContext(QueryDeclaration *assign_param, QueryDeclaration *left_param,
                                        QueryDeclaration *right_param) {
   std::string left_string = left_param->getString();
   std::string pattern_substring = right_param->getString();
@@ -74,10 +69,9 @@ void PatternStrategy::intersectContext(QueryDeclaration *assign_param,
   std::unordered_set<Entity *> valid_assign_entities = this->pkb_->getByPattern(left_string, pattern_substring);
 
   std::unordered_set<Entity *> intersected_results;
-  std::set_intersection(valid_assign_entities.begin(), valid_assign_entities.end(),
-                        assign_param->getContext().begin(), assign_param->getContext().end(),
+  std::set_intersection(valid_assign_entities.begin(), valid_assign_entities.end(), assign_param->getContext().begin(),
+                        assign_param->getContext().end(),
                         std::inserter(intersected_results, intersected_results.begin()));
 
   assign_param->setContext(intersected_results);
 }
-
--- Team14/Code14/src/spa/src/qps/query_evaluator/query_evaluator.h
+++ Team14/Code14/src/spa/src/qps/query_evaluator/query_evaluator.h
@@ -1,6 +1,7 @@
 // Copyright 2022 CS3203 Team14. All rights reserved.
 #pragma once
 
+#include <iterator>
 #include <unordered_map>
 #include <unordered_set>
 
