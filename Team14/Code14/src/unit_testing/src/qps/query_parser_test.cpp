7535e7a5c963b9e85233809a0d14a1888fe186a9
KelvinSoo <e0544272@u.nus.edu.sg>
2022-09-13 08:51:28 +0800

    feat: add more query parser tests

--- Team14/Code14/src/unit_testing/src/qps/query_parser_test.cpp
+++ Team14/Code14/src/unit_testing/src/qps/query_parser_test.cpp
@@ -5,24 +5,15 @@
 #include "qps/pql/query_call.h"
 #include "qps/query_parser.h"
 
-TEST(QueryParserTest, ValidTokenTest) {
-  std::vector<Token *> input_tokens_short = {new KeywordToken("assign"), new SymbolToken("v1"), new SemicolonToken(),
-                                             new KeywordToken("assign"), new SymbolToken("v2"), new SemicolonToken(),
-                                             new KeywordToken("Select"), new SymbolToken("v1"), new EndOfFileToken()};
-  QueryParser short_validator = QueryParser(input_tokens_short);
-  short_validator.parse();
-
-  ASSERT_EQ(short_validator.getDeclarations()[0]->getSynonym(), QuerySynonym("v1"));
-  ASSERT_EQ(short_validator.getDeclarations()[1]->getSynonym(), QuerySynonym("v2"));
-
+TEST(QueryParserTest, ValidSingleParentClauseTest) {
   std::vector<Token *> single_clause_parent_query = {
       new KeywordToken("assign"), new SymbolToken("v1"),   new SemicolonToken(),       new KeywordToken("assign"),
       new SymbolToken("v2"),      new SemicolonToken(),    new KeywordToken("Select"), new SymbolToken("v1"),
       new SymbolToken("such"),    new SymbolToken("that"), new KeywordToken("Parent"), new RoundOpenBracketToken(),
       new SymbolToken("v1"),      new CommaToken(),        new SymbolToken("v2"),      new RoundCloseBracketToken(),
       new EndOfFileToken()};
-  QueryParser long_validator = QueryParser(single_clause_parent_query);
-  long_validator.parse();
+  QueryParser parser = QueryParser(single_clause_parent_query);
+  parser.parse();
 
   QueryDeclaration* v1 = new AssignDeclaration(QuerySynonym("v1"));
   QueryDeclaration* v2 = new AssignDeclaration(QuerySynonym("v2"));
@@ -30,20 +21,52 @@ TEST(QueryParserTest, ValidTokenTest) {
                                SelectCall(v1, std::vector<QueryClause*> {new ParentClause(v1, v2)}));
 
   // check declarations
-  ASSERT_EQ(long_validator.getDeclarations()[0]->getSynonym(), v1->getSynonym());
-  ASSERT_EQ(long_validator.getDeclarations()[1]->getSynonym(), v2->getSynonym());
+  ASSERT_EQ(parser.getDeclarations()[0]->getSynonym(), v1->getSynonym());
+  ASSERT_EQ(parser.getDeclarations()[1]->getSynonym(), v2->getSynonym());
 
   // check call
-  ASSERT_EQ(long_validator.getQueryCalls()[0]->getType(), expected_query.getQueryCall().getType());
+  ASSERT_EQ(parser.getQueryCalls()[0]->getType(), expected_query.getQueryCall().getType());
 
   // check clause
-  ASSERT_EQ(long_validator.getQueryCalls()[0]->getClauseVector()[0]->getClauseType(),
+  ASSERT_EQ(parser.getQueryCalls()[0]->getClauseVector()[0]->getClauseType(),
             expected_query.getQueryCall().getClauseVector()[0]->getClauseType());
-  ASSERT_EQ(static_cast<ParentClause*>(long_validator.getQueryCalls()[0]->getClauseVector()[0])->getFirst()->getSynonym().toString(),
+  ASSERT_EQ(static_cast<ParentClause*>(parser.getQueryCalls()[0]->getClauseVector()[0])->getFirst()->getSynonym().toString(),
             static_cast<ParentClause*>(expected_query.getQueryCall().getClauseVector()[0])->getFirst()->getSynonym().toString());
-  ASSERT_EQ(static_cast<ParentClause*>(long_validator.getQueryCalls()[0]->getClauseVector()[0])->getSecond()->getSynonym().toString(),
+  ASSERT_EQ(static_cast<ParentClause*>(parser.getQueryCalls()[0]->getClauseVector()[0])->getSecond()->getSynonym().toString(),
             static_cast<ParentClause*>(expected_query.getQueryCall().getClauseVector()[0])->getSecond()->getSynonym().toString());
 }
+
+TEST(QueryParserTest, ValidSingleFollowsClauseTest) {
+  std::vector<Token *> single_clause_parent_query = {
+      new KeywordToken("assign"), new SymbolToken("v1"),   new SemicolonToken(),       new KeywordToken("assign"),
+      new SymbolToken("v2"),      new SemicolonToken(),    new KeywordToken("Select"), new SymbolToken("v1"),
+      new SymbolToken("such"),    new SymbolToken("that"), new KeywordToken("Follows"), new RoundOpenBracketToken(),
+      new SymbolToken("v1"),      new CommaToken(),        new SymbolToken("v2"),      new RoundCloseBracketToken(),
+      new EndOfFileToken()};
+  QueryParser parser = QueryParser(single_clause_parent_query);
+  parser.parse();
+
+  QueryDeclaration* v1 = new AssignDeclaration(QuerySynonym("v1"));
+  QueryDeclaration* v2 = new AssignDeclaration(QuerySynonym("v2"));
+  Query expected_query = Query(std::vector<QueryDeclaration*> {v1, v2},
+                               SelectCall(v1, std::vector<QueryClause*> {new FollowsClause(v1, v2)}));
+
+  // check declarations
+  ASSERT_EQ(parser.getDeclarations()[0]->getSynonym(), v1->getSynonym());
+  ASSERT_EQ(parser.getDeclarations()[1]->getSynonym(), v2->getSynonym());
+
+  // check call
+  ASSERT_EQ(parser.getQueryCalls()[0]->getType(), expected_query.getQueryCall().getType());
+
+  // check clause
+  ASSERT_EQ(parser.getQueryCalls()[0]->getClauseVector()[0]->getClauseType(),
+            expected_query.getQueryCall().getClauseVector()[0]->getClauseType());
+  ASSERT_EQ(static_cast<FollowsClause*>(parser.getQueryCalls()[0]->getClauseVector()[0])->getFirst()->getSynonym().toString(),
+            static_cast<FollowsClause*>(expected_query.getQueryCall().getClauseVector()[0])->getFirst()->getSynonym().toString());
+  ASSERT_EQ(static_cast<FollowsClause*>(parser.getQueryCalls()[0]->getClauseVector()[0])->getSecond()->getSynonym().toString(),
+            static_cast<FollowsClause*>(expected_query.getQueryCall().getClauseVector()[0])->getSecond()->getSynonym().toString());
+}
+
 
 TEST(QueryParserTest, InvalidTokenTest1) {
   std::vector<Token *> invalid_tokens = {new KeywordToken("var"),    new SymbolToken("v1"), new SemicolonToken(),
