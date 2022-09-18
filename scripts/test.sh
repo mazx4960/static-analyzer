22261b566eca5b46f3b8d33632380862fce6d30c
mazx4960 <desmondtan4561@gmail.com>
2022-09-18 22:41:58 +0800

    fix: add logs to system testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ test_names=("basic" "compute_ave" "compute_centroid")
 
 for test_name in "${test_names[@]}"; do
     echo "Running test $test_name"
-    build/src/autotester/autotester Team14/Tests14/system/"$test_name"_source.txt Team14/Tests14/system/"$test_name"_queries.txt test_results/"$test_name".xml > test_results/"$test_name".log
+    build/src/autotester/autotester Team14/Tests14/system/"$test_name"_source.txt Team14/Tests14/system/"$test_name"_queries.txt test_results/"$test_name".xml
     if grep -qiE "missing|additional|exception" test_results/"$test_name".xml; then
         echo "Test $test_name failed"
         exit 1
