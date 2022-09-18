7be8db8da8fe3b4d8ef1528ffa838023d9d6a7b9
Chen Xihao <chenxihao0627@gmail.com>
2022-09-18 16:03:23 +0800

    test: update script for basic source/query test

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,3 +9,4 @@ build/src/integration_testing/integration_testing
 # run system tests
 mkdir -p test_results
 build/src/autotester/autotester Team14/Tests14/system/Sample_source.txt Team14/Tests14/system/Sample_queries.txt test_results/Sample.xml
+build/src/autotester/autotester Team14/Tests14/system/basic_source.txt Team14/Tests14/system/basic_queries.txt test_results/basic.xml
