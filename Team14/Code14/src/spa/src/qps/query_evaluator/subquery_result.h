bc98edb715542ca1f492e5e17f1af349fe590429
Chen Xihao <chenxihao0627@gmail.com>
2022-09-19 11:56:12 +0800

    fix: pass by ref

--- Team14/Code14/src/spa/src/qps/query_evaluator/subquery_result.h
+++ Team14/Code14/src/spa/src/qps/query_evaluator/subquery_result.h
@@ -5,21 +5,29 @@
 
 #include "pkb/relationship/relationship_table.h"
 #include "qps/pql/query_declaration.h"
+using EntityPointerUnorderedMap = std::unordered_map<Entity *,
+                                                     EntityPointerUnorderedSet,
+                                                     EntityHashFunction,
+                                                     EntityPointerEquality>;
 
 class SubqueryResult {
  private:
   EntityPointerUnorderedMap table_;
+
   EntityPointerUnorderedMap table_inv_;
-  QueryDeclaration* first_decl_;
-  QueryDeclaration* second_decl_;
-  SubqueryResult(EntityPointerUnorderedMap, EntityPointerUnorderedMap, QueryDeclaration*, QueryDeclaration*);
+
+  QueryDeclaration *first_decl_;
+
+  QueryDeclaration *second_decl_;
+
+  SubqueryResult(EntityPointerUnorderedMap, EntityPointerUnorderedMap, QueryDeclaration *, QueryDeclaration *);
  public:
-  SubqueryResult(const EntityPointerUnorderedMap&, QueryDeclaration*, QueryDeclaration*);
+  SubqueryResult(const EntityPointerUnorderedMap &, QueryDeclaration *, QueryDeclaration *);
   [[nodiscard]] bool empty();
   SubqueryResult invert();
-  bool uses(QueryDeclaration* decl);
-  std::vector<QueryDeclaration *> getCommonSynonyms(const SubqueryResult& other);
-  EntityPointerUnorderedSet GetColumn(QuerySynonym*);
-  SubqueryResult Intersect(SubqueryResult other);
-  SubqueryResult Join(SubqueryResult other);
+  bool uses(QueryDeclaration *decl);
+  std::vector<QueryDeclaration *> getCommonSynonyms(const SubqueryResult &other);
+  EntityPointerUnorderedSet GetColumn(QuerySynonym *);
+  SubqueryResult Intersect(SubqueryResult &other);
+  SubqueryResult Join(SubqueryResult &other);
 };
