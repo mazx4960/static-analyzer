e7f85ac5c797bf4abbf6b31fc94e78a2e9ea4f74
Chen Xihao <chenxihao0627@gmail.com>
2022-09-26 15:17:23 +0800

    chore: clang

--- Team14/Code14/src/spa/src/commons/hash_combine.h
+++ Team14/Code14/src/spa/src/commons/hash_combine.h
@@ -8,16 +8,16 @@ template<class T>
  */
 class HashCombine {
  private:
-  static const size_t ADDED_VAL = 0x9e3779b9;
+  static const size_t kAddedVal = 0x9e3779b9;
 
-  static const size_t LEFT_SHIFT = 6;
+  static const size_t kLeftShift = 6;
 
-  static const size_t RIGHT_SHIFT = 2;
+  static const size_t kRightShift = 2;
 
   HashCombine() = default;
  public:
   static void hash_combine(std::size_t &seed, const T &val) {
     std::hash<T> hash_func;
-    seed ^= hash_func(val) + ADDED_VAL + (seed << LEFT_SHIFT) + (seed >> RIGHT_SHIFT);
+    seed ^= hash_func(val) + kAddedVal + (seed << kLeftShift) + (seed >> kRightShift);
   }
 };
