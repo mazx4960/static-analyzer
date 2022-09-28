4973ccecc6611b4eb6cdde28114167474bdbd359
Chen Xihao <chenxihao0627@gmail.com>
2022-09-28 15:51:36 +0800

    test: add invalid PQL exception test cases

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,17 +3,17 @@
 # run system tests
 mkdir -p test_results
 
-test_names=("basic" "compute_ave" "compute_centroid" "extreme_nesting")
+test_names=("pql_error", "basic" "compute_ave" "compute_centroid" "extreme_nesting")
 num_failed=0
 
 for test_name in "${test_names[@]}"; do
     echo "=================================================="
     echo "Running test '$test_name'"
     build/src/autotester/autotester Team14/Tests14/system/"$test_name"_source.txt Team14/Tests14/system/"$test_name"_queries.txt test_results/"$test_name".xml > test_results/"$test_name".log
-    if grep -qiE "missing|additional|exception" test_results/"$test_name".xml; then
+    if grep -qiE "missing|additional|exception|timeout" test_results/"$test_name".xml; then
         echo "Test '$test_name' failed"
         echo "--------------------------------------------------"
-        grep -iE -B 8 -A 1 "missing|additional|exception" test_results/"$test_name".log
+        grep -iE -B 8 -A 1 "missing|additional|exception|timeout" test_results/"$test_name".log
         num_failed=$((num_failed+1))
     else
         echo "Test '$test_name' passed"
