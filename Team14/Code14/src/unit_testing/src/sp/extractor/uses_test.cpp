6fa85ff6dc1e6048c33f83c17b5145dbf60ca8d9
mazx4960 <desmondtan4561@gmail.com>
2022-10-01 22:12:34 +0800

    fix: ut

--- Team14/Code14/src/unit_testing/src/sp/extractor/uses_test.cpp
+++ Team14/Code14/src/unit_testing/src/sp/extractor/uses_test.cpp
@@ -21,7 +21,6 @@ TEST(ExtractorTest, TestUses) {
 
   // DFS traversal should yield the following result
   std::vector<Relationship *> expected = {
-      new UsesRelationship(new ProcedureEntity("main"), new VariableEntity("v2")),
       new UsesRelationship(new AssignStmtEntity("1"), new VariableEntity("v2")),
   };
 
