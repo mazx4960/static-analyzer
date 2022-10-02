a9f35a659e54bee7fc8cb43be7e8fa74b18039e5
Smanmos <57220480+Smanmos@users.noreply.github.com>
2022-10-02 13:35:58 +0800

    feat: rewrite merging multiple subclauses

--- Team14/Code14/src/spa/src/qps/query_evaluator/result_projector.cpp
+++ Team14/Code14/src/spa/src/qps/query_evaluator/result_projector.cpp
@@ -19,58 +19,25 @@ EntityPointerUnorderedSet ResultProjector::intersect(const EntityPointerUnordere
  * @return set of Entity pointers of final result.
  */
 EntityPointerUnorderedSet ResultProjector::project() {
+
+  EntityPointerUnorderedSet candidates = this->called_declaration_->getContext();
+  QuerySynonym *synonym = this->called_declaration_->getSynonym();
   if (std::any_of(subquery_results_.begin(), subquery_results_.end(),
                   [](SubqueryResult subquery_result) {
                     return subquery_result.empty();
                   })) {
     return EntityPointerUnorderedSet();
   }
-
-  EntityPointerUnorderedSet candidates = this->called_declaration_->getContext();
-  QuerySynonym *synonym = this->called_declaration_->getSynonym();
-
-  switch (this->subquery_results_.size()) {
-    case 0: {
-      // Just return all possible results
-      return candidates;
-    }
-    case 1: {
-      if (this->subquery_results_[0].uses(this->called_declaration_)) {
-        return this->subquery_results_[0].GetColumn(synonym);
-      }
-      return candidates;
-    }
-    case 2: {
-      std::vector<QueryDeclaration *> common_synonyms = this->subquery_results_[0].getCommonSynonyms(this->subquery_results_[1]);
-      switch (common_synonyms.size()) {
-        case 0: {
-          for (auto res : this->subquery_results_) {
-            if (res.uses(this->called_declaration_)) {
-              candidates = ResultProjector::intersect(candidates, res.GetColumn(synonym));
-            }
-          }
-          return candidates;
-        }
-        case 1: {
-          if (*common_synonyms[0]->getSynonym() == *synonym) {
-            for (auto res : this->subquery_results_) {
-              if (res.uses(this->called_declaration_)) {
-                candidates = ResultProjector::intersect(candidates, res.GetColumn(synonym));
-              }
-            }
-            return candidates;
-          }
-          SubqueryResult join = this->subquery_results_[0].Join(this->subquery_results_[1]);
-          if (join.uses(this->called_declaration_)) { return join.GetColumn(synonym); }
-          return this->called_declaration_->getContext();
-        }
-        case 2: {
-          SubqueryResult intersection = this->subquery_results_[0].Intersect(this->subquery_results_[1]);
-          if (intersection.uses(this->called_declaration_)) { return intersection.GetColumn(synonym); }
-          return candidates;
-        }
-      }
-    }
+  if (std::none_of(subquery_results_.begin(), subquery_results_.end(),
+                  [synonym](SubqueryResult subquery_result) {
+                    return subquery_result.uses(synonym);
+                  })) {
+    return EntityPointerUnorderedSet();
   }
-  return EntityPointerUnorderedSet();
+
+  SubqueryResult intermediate_result = subquery_results_[0];
+  for (auto result = subquery_results_.begin() + 1; result != subquery_results_.end(); result++) {
+    intermediate_result = intermediate_result.Join(*result);
+  }
+  return intermediate_result.GetColumn(synonym);
 }
