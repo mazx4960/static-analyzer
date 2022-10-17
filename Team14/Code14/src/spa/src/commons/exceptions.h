60ebc5046f7940fd2eb77bda67bdc91a92f3014c
KelvinSoo <e0544272@u.nus.edu.sg>
2022-10-17 13:55:19 +0800

    fix: exceptions

--- Team14/Code14/src/spa/src/commons/exceptions.h
+++ Team14/Code14/src/spa/src/commons/exceptions.h
@@ -5,14 +5,14 @@
 
 class SyntaxError : public std::runtime_error {
  public:
-  SyntaxError(const std::string &message) : std::runtime_error(
+  explicit SyntaxError(const std::string &message) : std::runtime_error(
       "Syntax Error: " + message) {
   }
 };
 
 class SemanticError : public std::runtime_error {
  public:
-  SemanticError(const std::string &message) : std::runtime_error(
+  explicit SemanticError(const std::string &message) : std::runtime_error(
       "SemanticError Error: " + message) {
   }
 };
--- Team14/Code14/src/spa/src/commons/lexer/lexer_exceptions.h
+++ Team14/Code14/src/spa/src/commons/lexer/lexer_exceptions.h
@@ -2,6 +2,7 @@
 
 #include <stdexcept>
 #include <string>
+#include "commons/exceptions.h"
 
 class LexSyntaxError : public SyntaxError {
  public:
