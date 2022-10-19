dfcc803dc83f6b69b6cfa26808cd44b0dfff6fff
mazx4960 <desmondtan4561@gmail.com>
2022-10-19 20:34:24 +0800

    chore: update profile statistics

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -14,5 +14,6 @@ for test_name in "${test_names[@]}"; do
     echo "Profiling '$test_name'"
     python3 run_with_mprof.py -c build/src/autotester/autotester $test_folder/"$test_name"_source.txt $test_folder/"$test_name"_queries.txt $result_folder/"$test_name".xml -t 0.001
     echo "[PARSE TIME]" $(sed -n -E 's/.*<parsing_time_taken>(.+)<\/parsing_time_taken>/\1/p' $result_folder/"$test_name".xml)
+    echo "[MAX QUERY TIME]" $(sed -n -E 's/<time_taken>(.+)<\/time_taken>/\1/p' $result_folder/"$test_name".xml | sort -r | head -n 1)
     echo "=============================================================="
 done
